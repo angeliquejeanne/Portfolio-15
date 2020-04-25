b06f3b43186fbd96f1cb19299fe2e5915c878930
Angelique Simon <anggiie0212@gmail.com>
2020-04-26 01:04:32 +0200

    add js section testimonials, attention bug

--- index.js
+++ index.js
@@ -54,3 +54,89 @@ const galleryItem=gallery.querySelectorAll(".item");
             lightboxImage.src=element.querySelector("img").getAttribute("src")
         })
     })
+
+//Testimonial slider
+const sliderContainer=document.querySelector(".testi-slider");
+const slides=sliderContainer.children;
+const containerWidht=sliderContainer.offsetWidth;
+const margin=15;
+let itemPerSlide=0;
+let slideDots;
+
+const responsive=[
+    {breakPoint:{width:0, item:1}}, 
+    {breakPoint:{width:991, item:2}} 
+]
+
+function load()
+{
+    for(let i=0; i<responsive.length; i++)
+    {
+        if(window.innerWidth>responsive[i].breakPoint.width)
+        {
+            itemPerSlide=responsive[i].breakPoint.item;
+        }
+    }
+    start();
+}
+function start()
+{
+    totalWidht=0;
+    for(let i=0; i<slides.length; i++)
+    {
+        slides[i].style.width=(containerWidht/itemPerSlide)-margin + "px";
+        slides[i].style.margin=margin/2 + "px";
+        totalWidht+=containerWidht/itemPerSlide;
+    }
+    sliderContainer.style.width=totalWidht + "px";
+    slideDots=Math.ceil(slides.length/itemPerSlide);
+
+    for(let i=0; i<slideDots; i++)
+    {
+        const div=document.createElement("div");
+        div.id=i;
+        div.setAttribute("onclick","controlSlide(this)");
+        if(i==0)
+        {
+            div.classList.add("active");
+        }
+        document.querySelector(".slide-controls").appendChild(div);
+    }
+
+}
+let currentSlide=0;
+let autoSlide=0;
+
+function controlSlide(element){
+    clearInterval(timer)
+    timer=setInterval(autoPlay,5000);
+    autoSlide=element.id;
+currentSlide=element.id;
+changeSlide(currentSlide)
+}
+
+function changeSlide(currentSlide){
+    controlButtons=document.querySelector(".slide-controls").children;
+
+    for(let i=0; i<controlButtons.length; i++){
+            controlButtons[i].classList.remove("active")
+    }
+    controlButtons[currentSlide].classList.add("active")
+
+    sliderContainer.style.marginLeft=-(containerWidth*currentSlide) + "px";
+}
+function autoPlay(){
+    console.log("call")
+    if(autoSlide==slideDots-1){
+        autoSlide=0;
+    }
+    else{
+        autoSlide++;
+    }
+    changeSlide(autoSlide);
+}
+let timer=setInterval(autoPlay,5000);
+
+
+window.onload=load();
+
